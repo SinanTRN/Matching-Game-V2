080b616a401aa2e036ae77ac79d79fd7b810366a
Sinan Turan <turansinan2004@gmail.com>
2024-05-30 21:55:05 +0300

    SinglePlayer kolay seviye tasarımı oluşturuldu.Random simgeler atandı.

---
+++ Matching-Game-V2/SinglePlayer.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Matching_Game_V2
+{
+    partial class SinglePlayer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            i16 = new Label();
+            i15 = new Label();
+            i14 = new Label();
+            i13 = new Label();
+            i12 = new Label();
+            i11 = new Label();
+            i10 = new Label();
+            i8 = new Label();
+            i7 = new Label();
+            i6 = new Label();
+            i5 = new Label();
+            i4 = new Label();
+            i3 = new Label();
+            i1 = new Label();
+            i2 = new Label();
+            i9 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            btnBasla = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.MediumTurquoise;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(i16, 3, 3);
+            tableLayoutPanel1.Controls.Add(i15, 2, 3);
+            tableLayoutPanel1.Controls.Add(i14, 1, 3);
+            tableLayoutPanel1.Controls.Add(i13, 0, 3);
+            tableLayoutPanel1.Controls.Add(i12, 3, 2);
+            tableLayoutPanel1.Controls.Add(i11, 2, 2);
+            tableLayoutPanel1.Controls.Add(i10, 1, 2);
+            tableLayoutPanel1.Controls.Add(i8, 3, 1);
+            tableLayoutPanel1.Controls.Add(i7, 2, 1);
+            tableLayoutPanel1.Controls.Add(i6, 1, 1);
+            tableLayoutPanel1.Controls.Add(i5, 0, 1);
+            tableLayoutPanel1.Controls.Add(i4, 3, 0);
+            tableLayoutPanel1.Controls.Add(i3, 2, 0);
+            tableLayoutPanel1.Controls.Add(i1, 0, 0);
+            tableLayoutPanel1.Controls.Add(i2, 1, 0);
+            tableLayoutPanel1.Controls.Add(i9, 0, 2);
+            tableLayoutPanel1.Location = new Point(25, 44);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(429, 365);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // i16
+            // 
+            i16.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i16.Location = new Point(323, 272);
+            i16.Name = "i16";
+            i16.Size = new Size(98, 91);
+            i16.TabIndex = 14;
+            i16.Text = "c";
+            i16.TextAlign = ContentAlignment.MiddleCenter;
+            i16.Click += i1_Click;
+            // 
+            // i15
+            // 
+            i15.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i15.Location = new Point(217, 272);
+            i15.Name = "i15";
+            i15.Size = new Size(98, 91);
+            i15.TabIndex = 13;
+            i15.Text = "c";
+            i15.TextAlign = ContentAlignment.MiddleCenter;
+            i15.Click += i1_Click;
+            // 
+            // i14
+            // 
+            i14.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i14.Location = new Point(111, 272);
+            i14.Name = "i14";
+            i14.Size = new Size(98, 91);
+            i14.TabIndex = 12;
+            i14.Text = "c";
+            i14.TextAlign = ContentAlignment.MiddleCenter;
+            i14.Click += i1_Click;
+            // 
+            // i13
+            // 
+            i13.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i13.Location = new Point(5, 272);
+            i13.Name = "i13";
+            i13.Size = new Size(98, 91);
+            i13.TabIndex = 11;
+            i13.Text = "c";
+            i13.TextAlign = ContentAlignment.MiddleCenter;
+            i13.Click += i1_Click;
+            // 
+            // i12
+            // 
+            i12.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i12.Location = new Point(323, 182);
+            i12.Name = "i12";
+            i12.Size = new Size(98, 88);
+            i12.TabIndex = 10;
+            i12.Text = "c";
+            i12.TextAlign = ContentAlignment.MiddleCenter;
+            i12.Click += i1_Click;
+            // 
+            // i11
+            // 
+            i11.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i11.Location = new Point(217, 182);
+            i11.Name = "i11";
+            i11.Size = new Size(98, 88);
+            i11.TabIndex = 9;
+            i11.Text = "c";
+            i11.TextAlign = ContentAlignment.MiddleCenter;
+            i11.Click += i1_Click;
+            // 
+            // i10
+            // 
+            i10.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i10.Location = new Point(111, 182);
+            i10.Name = "i10";
+            i10.Size = new Size(98, 88);
+            i10.TabIndex = 8;
+            i10.Text = "c";
+            i10.TextAlign = ContentAlignment.MiddleCenter;
+            i10.Click += i1_Click;
+            // 
+            // i8
+            // 
+            i8.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i8.Location = new Point(323, 92);
+            i8.Name = "i8";
+            i8.Size = new Size(98, 88);
+            i8.TabIndex = 6;
+            i8.Text = "c";
+            i8.TextAlign = ContentAlignment.MiddleCenter;
+            i8.Click += i1_Click;
+            // 
+            // i7
+            // 
+            i7.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i7.Location = new Point(217, 92);
+            i7.Name = "i7";
+            i7.Size = new Size(98, 88);
+            i7.TabIndex = 5;
+            i7.Text = "c";
+            i7.TextAlign = ContentAlignment.MiddleCenter;
+            i7.Click += i1_Click;
+            // 
+            // i6
+            // 
+            i6.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i6.Location = new Point(111, 92);
+            i6.Name = "i6";
+            i6.Size = new Size(98, 88);
+            i6.TabIndex = 4;
+            i6.Text = "c";
+            i6.TextAlign = ContentAlignment.MiddleCenter;
+            i6.Click += i1_Click;
+            // 
+            // i5
+            // 
+            i5.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i5.Location = new Point(5, 92);
+            i5.Name = "i5";
+            i5.Size = new Size(98, 88);
+            i5.TabIndex = 3;
+            i5.Text = "c";
+            i5.TextAlign = ContentAlignment.MiddleCenter;
+            i5.Click += i1_Click;
+            // 
+            // i4
+            // 
+            i4.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i4.Location = new Point(323, 2);
+            i4.Name = "i4";
+            i4.Size = new Size(98, 88);
+            i4.TabIndex = 2;
+            i4.Text = "c";
+            i4.TextAlign = ContentAlignment.MiddleCenter;
+            i4.Click += i1_Click;
+            // 
+            // i3
+            // 
+            i3.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i3.Location = new Point(217, 2);
+            i3.Name = "i3";
+            i3.Size = new Size(98, 88);
+            i3.TabIndex = 1;
+            i3.Text = "c";
+            i3.TextAlign = ContentAlignment.MiddleCenter;
+            i3.Click += i1_Click;
+            // 
+            // i1
+            // 
+            i1.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i1.Location = new Point(5, 2);
+            i1.Name = "i1";
+            i1.Size = new Size(98, 88);
+            i1.TabIndex = 0;
+            i1.Text = "c";
+            i1.TextAlign = ContentAlignment.MiddleCenter;
+            i1.Click += i1_Click;
+            // 
+            // i2
+            // 
+            i2.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i2.Location = new Point(111, 2);
+            i2.Name = "i2";
+            i2.Size = new Size(98, 88);
+            i2.TabIndex = 0;
+            i2.Text = "c";
+            i2.TextAlign = ContentAlignment.MiddleCenter;
+            i2.Click += i1_Click;
+            // 
+            // i9
+            // 
+            i9.Font = new Font("Webdings", 36F, FontStyle.Bold, GraphicsUnit.Point, 2);
+            i9.Location = new Point(5, 182);
+            i9.Name = "i9";
+            i9.Size = new Size(98, 88);
+            i9.TabIndex = 7;
+            i9.Text = "c";
+            i9.TextAlign = ContentAlignment.MiddleCenter;
+            i9.Click += i1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 162);
+            label1.Location = new Point(543, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 30);
+            label1.TabIndex = 1;
+            label1.Text = "Süre";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(605, 51);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(128, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // btnBasla
+            // 
+            btnBasla.BackColor = Color.PaleTurquoise;
+            btnBasla.Location = new Point(526, 371);
+            btnBasla.Name = "btnBasla";
+            btnBasla.Size = new Size(104, 38);
+            btnBasla.TabIndex = 3;
+            btnBasla.Text = "Başla";
+            btnBasla.UseVisualStyleBackColor = false;
+            btnBasla.Click += btnBasla_Click;
+            // 
+            // SinglePlayer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Beige;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnBasla);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(tableLayoutPanel1);
+            Name = "SinglePlayer";
+            Text = "SinglePlayer";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button btnBasla;
+        private Label i1;
+        private Label i16;
+        private Label i15;
+        private Label i14;
+        private Label i13;
+        private Label i12;
+        private Label i11;
+        private Label i10;
+        private Label i9;
+        private Label i8;
+        private Label i7;
+        private Label i6;
+        private Label i5;
+        private Label i4;
+        private Label i3;
+        private Label i2;
+    }
+}
---
+++ Matching-Game-V2/SinglePlayer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matching_Game_V2
+{
+    public partial class SinglePlayer : Form
+    {
+        Random random = new Random();
+        List<string> simgeler;
+        List<Label> labelList;
+        Label firstClicked;
+        Label secondClicked;
+        int sure;
+        int sayac = 0;
+        int total = 0;
+        public SinglePlayer()
+        {
+            InitializeComponent();
+            InitializeLabelList();
+            InitializeIcons();
+        }
+        private void InitializeIcons()
+        {
+            simgeler = new List<string>(){
+                            "!", "!", "N", "N", ",", ",", "k", "k",
+                            "b", "b", "v", "v", "w", "w", "z", "z",
+                            };
+        }
+        private void labelleriSifirla()
+        {
+            foreach (Label label in labelList)
+            {
+                label.Text = "";
+            }
+        }
+        private void InitializeLabelList()
+        {
+            labelList = new List<Label>();
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Label label && ShouldAddLabel(label))
+                {
+                    labelList.Add(label);
+                }
+            }
+        }
+        private bool ShouldAddLabel(Label label)
+        {
+            // "i1" den "i36" ya kadar olan labelleri filtreleme
+            if (label.Name.StartsWith("i"))
+            {
+                if (int.TryParse(label.Name.Substring(1), out int number))
+                {
+                    return number >= 1 && number <= 16;
+                }
+            }
+            return false;
+        }
+        private void randomAta()
+        {
+            foreach (Label label in labelList)
+            {
+                int rastgele = random.Next(simgeler.Count);
+                label.Text = simgeler[rastgele];
+                //label.ForeColor = label.BackColor;
+                simgeler.RemoveAt(rastgele);
+            }
+            InitializeIcons();
+        }
+        private void i1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            randomAta();
+        }
+    }
+}
